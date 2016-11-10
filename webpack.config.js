47936d36f4b264d36d150094d99bb8e1f9fd1ea5
VHero <a772839689@gmail.com>
2016-11-10 09:16:34 +0800

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
 module.exports = {
-    devtool: 'eval-source-map', //配置生成Source Maps，选择合适的选项
-    entry: __dirname + "/app/main.js", //入口文件
-    output: {
-        path: __dirname + "/public/", //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
-    },
-    module: { //在配置文件里添加JSON loader
-        loaders: [{
-            test: /\.json$/,
-            loader: "json"
-        }]
-    },
-    devServer: {
-        contentBase: __dirname+"/public", //本地服务器所加载的页面所在的目录
-        colors: true, //终端中输出结果为彩色
-        historyApiFallback: true, //不跳转
-        inline: true //实时刷新	
-    }
-}
+  devtool: 'eval-source-map',
 
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js",
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015','react']
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
+}
